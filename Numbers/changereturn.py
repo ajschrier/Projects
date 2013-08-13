d0b45f209fd919a343289918305b341c37068519
AJ Schrier <ajschrier@gmail.com>
2013-08-13 00:54:31 -0700

    added changereturn, edited mortgage

---
+++ Numbers/changereturn.py
@@ -0,0 +1,10 @@
+total=input("Enter the total for the purchase: ")
+payment=input("Enter the amount paid: ")
+
+change = payment - total
+
+dollars=int(change)
+
+cointotal=(change%dollars) * 100
+
+print int(cointotal)
--- Numbers/mortgage.py
+++ Numbers/mortgage.py
@@ -2,13 +2,25 @@ apr=input("What is the APR of the loan?")
 termlength=input("What is the length of the loan (in years)?")
 loanamount=input("What is the amount of the loan?")
 
-n=termlength/12
+n=float(termlength)/12
 c=apr/12
 
-# def payment(l,c,n):
-# 	return (l * (c(1+c)^n) / ((1-c)^n - 1))
-# 	
-# print payment(int(loanamount), c, n)
+def payment(l,c,n):
+	return (l * (c(1+c)^n) / ((1-c)^n - 1))
+	
+# print payment(loanamount, c, n)
+# print type(loanamount)
+# print type(c)
+# print type(n)
+# 
+# def call(l,c,n):
+# 	print l
+# 	print c
+# 	print n
+# 
+# call(loanamount,c,n)
 
+def ret(l):
+	return l
 
-print c
+print ret(loanamount)
