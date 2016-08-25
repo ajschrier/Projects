0c79fd6abe20a821dade42867a4a4e75fbc56ccb
AJ Schrier <aschrier@apple.com>
2016-08-24 18:59:45 -0700

    changereturn: flake8 edits

--- Numbers/changereturn.py
+++ Numbers/changereturn.py
@@ -1,10 +1,10 @@
-total=input("Enter the total for the purchase: ")
-payment=input("Enter the amount paid: ")
+total = input("Enter the total for the purchase: ")
+payment = input("Enter the amount paid: ")
 
 change = payment - total
 
-dollars=int(change)
+dollars = int(change)
 
-cointotal=(change%dollars) * 100
+cointotal = (change % dollars) * 100
 
 print int(cointotal)
