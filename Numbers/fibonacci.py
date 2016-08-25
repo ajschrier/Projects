bce02532bc8d28f5c3c9d2a8e1d0cca2a7b5398d
AJ Schrier <aschrier@apple.com>
2016-08-24 17:38:43 -0700

    fibonacci: flake8 corrections

--- Numbers/fibonacci.py
+++ Numbers/fibonacci.py
@@ -1,13 +1,13 @@
 import sys
 import time
 
-n=int(sys.argv[1])
+n = int(sys.argv[1])
 
-series=[0,1]
+series = [0, 1]
 
 while len(series) < n:
-	series.append(series[-1] + series [-2])
+    series.append(series[-1] + series[-2])
 
-for i in range (0, len(series)):
-	print series[i]
-	time.sleep(0.2)
+for i in range(0, len(series)):
+    print series[i]
+    time.sleep(0.2)
