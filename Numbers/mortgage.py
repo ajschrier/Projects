bc5e160d1f79c0d68644ec1d2f8fc44ef6e58ece
AJ Schrier <ajschrier@gmail.com>
2016-08-27 01:06:05 -0700

    mortgage: flake8 edits and removed commented out code

--- Numbers/mortgage.py
+++ Numbers/mortgage.py
@@ -1,26 +1,16 @@
-apr=input("What is the APR of the loan?")
-termlength=input("What is the length of the loan (in years)?")
-loanamount=input("What is the amount of the loan?")
+apr = input("What is the APR of the loan?")
+termlength = input("What is the length of the loan (in years)?")
+loanamount = input("What is the amount of the loan?")
 
-n=float(termlength)/12
-c=apr/12
+n = float(termlength)/12
+c = apr/12
 
-def payment(l,c,n):
-	return (l * (c(1+c)^n) / ((1-c)^n - 1))
-	
-# print payment(loanamount, c, n)
-# print type(loanamount)
-# print type(c)
-# print type(n)
-# 
-# def call(l,c,n):
-# 	print l
-# 	print c
-# 	print n
-# 
-# call(loanamount,c,n)
+
+def payment(l, c, n):
+    return (l * (c(1+c) ^ n) / ((1-c) ^ n - 1))
+
 
 def ret(l):
-	return l
+    return l
 
 print ret(loanamount)
