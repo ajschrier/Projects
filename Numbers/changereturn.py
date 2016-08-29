60e47d5cc1e626071e2332e7e8ff262cb7cfe31a
AJ Schrier <aschrier@apple.com>
2016-08-29 15:01:42 -0700

    changereturn: added extra output

--- Numbers/changereturn.py
+++ Numbers/changereturn.py
@@ -1,3 +1,8 @@
+'''
+The user enters a cost and then the amount of money given. The program will
+figure out the change and the number of quarters, dimes, nickels, pennies
+needed for the change.
+'''
 total = input("Enter the total for the purchase: ")
 payment = input("Enter the amount paid: ")
 
@@ -7,4 +12,7 @@ dollars = int(change)
 
 cointotal = (change % dollars) * 100
 
-print int(cointotal)
+print "Total: {0}".format(float(total))
+print "Paid: {0}".format(float(payment))
+
+print "Change due: {0}".format(int(cointotal))
